0441ead82ac6fa8b3b1b97fbfd3e7df3288411e2
Allysamade <Allysasd29@gmail.com>
2019-05-03 00:45:25 -0700

    Commit pertama kali

---
+++ app/src/main/java/com/allysa/android_swipeandrefreshlayout/MainActivity.java
@@ -0,0 +1,8 @@
+package com.allysa.android_swipeandrefreshlayout;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
